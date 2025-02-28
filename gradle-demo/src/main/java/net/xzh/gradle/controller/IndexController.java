8d6b7bba54f8deca2cc9222fd091cc3410a59bff
xzh <xcg992224@163.com>
2025-02-28 09:20:50 +0800

    update

--- gradle-demo/src/main/java/net/xzh/gradle/controller/IndexController.java
+++ gradle-demo/src/main/java/net/xzh/gradle/controller/IndexController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
-	/**
-	 * 登录页
-	 */
-	@RequestMapping(value = "/index")
-	@ResponseBody
-	public String index() {
-		return "这是一个gradle demo。"+ System.currentTimeMillis();
-	}
+    /**
+     * 登录页
+     */
+    @RequestMapping(value = "/index")
+    @ResponseBody
+    public String index() {
+        return "这是一个gradle demo。" + System.currentTimeMillis();
+    }
 
 }
