00a1f41903acf9a5b74cc5579cf3731c80f96179
xzh <xcg992224@163.com>
2025-03-05 11:40:12 +0800

    add debug log

--- spring-ai-function/src/main/java/net/xzh/fun/controller/FunctionCallController.java
+++ spring-ai-function/src/main/java/net/xzh/fun/controller/FunctionCallController.java
@@ -1,15 +1,15 @@
 package net.xzh.fun.controller;
 
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 import reactor.core.publisher.Flux;
 
 
